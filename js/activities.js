e62f5c7e3c6d8d46970e54e6d1cd487556a96516
yangankohla0 <yanga.nkohla@yahoo.com>
2019-05-26 13:22:37 +0200

    Accomadating for screens with min-width:1368px wide or more

--- js/activities.js
+++ js/activities.js
@@ -1,12 +1,12 @@
 // ${name}'s activities
-var activities = [ "code", "read", "google things", "drink coffee", "git add", "git commit", "git push", "play", "introvert", "otaku",  "nap" ];
+var activities = [ "code", "read", "google things", "drink coffee", "git add", "play", "introvert", "git push", "nap", "git commit" ];
 var counter = 0;
 var elem = document.getElementById("activities");
 var inst = setInterval(change, 2000);
 
 function change() {
   elem.innerHTML = activities[counter];
-  elem.style.backgroundColor = "#D3D3D3";
+  // elem.style.backgroundColor = "#D3D3D3";
   counter++;
   if (counter >= activities.length) {
     counter = 0;
